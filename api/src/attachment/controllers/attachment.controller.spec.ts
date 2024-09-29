e11fb8b632fce0053b94c1f55a544129da891cb6
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2024-09-29 08:36:33 +0100

    fix: remove non compliant OSD license term

--- api/src/attachment/controllers/attachment.controller.spec.ts
+++ api/src/attachment/controllers/attachment.controller.spec.ts
@@ -4,7 +4,6 @@
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
- * 3. SaaS Restriction: This software, or any derivative of it, may not be used to offer a competing product or service (SaaS) without prior written consent from Hexastack. Offering the software as a service or using it in a commercial cloud environment without express permission is strictly prohibited.
  */
 
 import { BadRequestException } from '@nestjs/common/exceptions';
