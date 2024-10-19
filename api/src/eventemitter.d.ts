39d8d65f828e292498de75d3a19e8875f4dd5a74
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-10-19 11:33:15 +0100

    fix(api): decentralize extensions typing

--- api/src/eventemitter.d.ts
+++ api/src/eventemitter.d.ts
@@ -34,7 +34,6 @@ import {
   type NlpValue,
 } from '@/nlp/schemas/nlp-value.schema';
 import { type Setting } from '@/setting/schemas/setting.schema';
-import type { CheckboxSetting, TextSetting } from '@/setting/schemas/types';
 import { type Invitation } from '@/user/schemas/invitation.schema';
 import { type Model } from '@/user/schemas/model.schema';
 import { type Permission } from '@/user/schemas/permission.schema';
@@ -54,17 +53,7 @@ declare module '@nestjs/event-emitter' {
     operations: O;
   }
 
-  interface IHookExtensionsOperationMap {
-    messenger: TDefinition<
-      object,
-      {
-        get_started_button: Setting;
-        access_token: Setting;
-        composer_input_disabled: CheckboxSetting;
-        greeting_text: TextSetting;
-      }
-    >;
-  }
+  interface IHookExtensionsOperationMap {}
 
   interface IHookSettingsGroupLabelOperationMap {
     chatbot_settings: TDefinition<
