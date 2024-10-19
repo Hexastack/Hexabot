d3e0c2e28722c3e25a10e789ba6d33a0f8522352
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-10-19 13:34:23 +0100

    feat(widget): automate adding copyright header

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,32 +1,39 @@
+/*
+ * Copyright © 2024 Hexastack. All rights reserved.
+ *
+ * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
+ * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
+ * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
+ */
+
 import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), dts()],
     server: {
-      host: '0.0.0.0',
+      host: "0.0.0.0",
     },
     define: {
-      'process.env':
-        mode === 'development' ? { 'process.env': process.env } : {},
+      "process.env":
+        mode === "development" ? { "process.env": process.env } : {},
     },
     build: {
       lib: {
-        entry: resolve(__dirname, 'src/ChatWidget.tsx'),
-        name: 'HexabotWidget',
+        entry: resolve(__dirname, "src/ChatWidget.tsx"),
+        name: "HexabotWidget",
         fileName: (format) => `hexabot-widget.${format}.js`,
       },
       rollupOptions: {
-        external: ['react', 'react-dom'],
+        external: ["react", "react-dom"],
         output: {
           globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
+            react: "React",
+            "react-dom": "ReactDOM",
           },
         },
       },
