5218233434b07555a899d361496eef60b73fd01d
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2024-10-22 14:37:42 +0100

    feat: rename live chat tester to console

--- api/src/extensions/channels/console/settings.ts
+++ api/src/extensions/channels/console/settings.ts
@@ -12,83 +12,83 @@ import { SettingType } from '@/setting/schemas/types';
 
 import { Web } from '../web/types';
 
-export const LIVE_CHAT_TEST_CHANNEL_NAME = 'live-chat-tester-channel';
+export const CONSOLE_CHANNEL_NAME = 'console-channel';
 
-export const LIVE_CHAT_TEST_CHANNEL_NAMESPACE = 'live_chat_tester_channel';
+export const CONSOLE_CHANNEL_NAMESPACE = 'console_channel';
 
 export default [
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.verification_token,
     value: 'test',
     type: SettingType.text,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.allowed_domains,
     value: config.frontendPath,
     type: SettingType.text,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.start_button,
     value: true,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.input_disabled,
     value: false,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.persistent_menu,
     value: true,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.greeting_message,
     value: 'Welcome! Ready to start a conversation with our chatbot?',
     type: SettingType.textarea,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.theme_color,
     value: 'teal',
     type: SettingType.select,
     options: ['teal', 'orange', 'red', 'green', 'blue', 'dark'],
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.show_emoji,
     value: true,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.show_file,
     value: true,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.show_location,
     value: true,
     type: SettingType.checkbox,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.allowed_upload_size,
     value: 2500000,
     type: SettingType.number,
   },
   {
-    group: LIVE_CHAT_TEST_CHANNEL_NAMESPACE,
+    group: CONSOLE_CHANNEL_NAMESPACE,
     label: Web.SettingLabel.allowed_upload_types,
     value:
       'audio/mpeg,audio/x-ms-wma,audio/vnd.rn-realaudio,audio/x-wav,image/gif,image/jpeg,image/png,image/tiff,image/vnd.microsoft.icon,image/vnd.djvu,image/svg+xml,text/css,text/csv,text/html,text/plain,text/xml,video/mpeg,video/mp4,video/quicktime,video/x-ms-wmv,video/x-msvideo,video/x-flv,video/web,application/msword,application/vnd.ms-powerpoint,application/pdf,application/vnd.ms-excel,application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.tex,application/vnd.oasis.opendocument.spreadsheet,application/vnd.oasis.opendocument.graphics,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.openxmlformats-officedocument.wordprocessingml.document',
     type: SettingType.textarea,
   },
-] as const satisfies ChannelSetting<typeof LIVE_CHAT_TEST_CHANNEL_NAME>[];
+] as const satisfies ChannelSetting<typeof CONSOLE_CHANNEL_NAME>[];
