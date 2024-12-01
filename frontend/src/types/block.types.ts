dfde924b724dc219c3eeba1d24d427267699f15b
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-12-01 09:37:00 +0100

    fix: remove unused effect comment

--- frontend/src/types/block.types.ts
+++ frontend/src/types/block.types.ts
@@ -49,7 +49,6 @@ export interface BlockOptions {
   // Only if the block has next blocks
   fallback?: BlockFallbackOptions;
   assignTo?: string;
-  // plugins effects
 }
 
 export type BlockMessage =
