8bb6b05f449610a15fa45b2374d84a218155ab9c
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2024-12-10 18:51:51 +0100

    fix: channel + event wrapper typing

--- api/src/chat/schemas/types/channel.ts
+++ api/src/chat/schemas/types/channel.ts
@@ -10,12 +10,12 @@ import { ChannelName } from '@/channel/types';
 
 export type SubscriberChannelData<
   C extends ChannelName = null,
-  K extends keyof SubscriberChannelDict[C] = keyof SubscriberChannelDict[C],
+  // K extends keyof SubscriberChannelDict[C] = keyof SubscriberChannelDict[C],
 > = C extends null
   ? { name: ChannelName }
   : {
       name: C;
     } & {
       // Channel's specific attributes
-      [P in keyof SubscriberChannelDict[C]]: SubscriberChannelDict[C][K];
+      [P in keyof SubscriberChannelDict[C]]: SubscriberChannelDict[C][P];
     };
