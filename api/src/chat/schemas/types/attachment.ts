d48b88f41e74bf875affd62cc64044bc88307c2c
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2025-01-09 10:52:00 +0100

    fix: remove unecessary WithUrl typing

--- api/src/chat/schemas/types/attachment.ts
+++ api/src/chat/schemas/types/attachment.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -21,10 +21,8 @@ export type AttachmentForeignKey = {
   attachment_id: string;
 };
 
-export type WithUrl<A> = A & { url?: string };
-
 export interface AttachmentPayload<
-  A extends WithUrl<Attachment> | AttachmentForeignKey,
+  A extends Attachment | AttachmentForeignKey,
 > {
   type: FileType;
   payload: A;
--- frontend/src/components/inbox/components/AttachmentViewer.tsx
+++ frontend/src/components/inbox/components/AttachmentViewer.tsx
@@ -1,10 +1,11 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
+
 
 import DownloadIcon from "@mui/icons-material/Download";
 import { Button, Dialog, DialogContent } from "@mui/material";
@@ -18,7 +19,6 @@ import {
   FileType,
   StdIncomingAttachmentMessage,
   StdOutgoingAttachmentMessage,
-  WithUrl,
 } from "@/types/message.types";
 
 interface AttachmentInterface {
@@ -95,7 +95,7 @@ const componentMap: { [key in FileType]: FC<AttachmentInterface> } = {
 export const AttachmentViewer = (props: {
   message:
     | StdIncomingAttachmentMessage
-    | StdOutgoingAttachmentMessage<WithUrl<AttachmentAttrs>>;
+    | StdOutgoingAttachmentMessage<AttachmentAttrs>;
 }) => {
   const message = props.message;
 
