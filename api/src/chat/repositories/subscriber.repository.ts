341ee5aea2ae2183e2083fcd8daaef1575041a68
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-01-13 16:16:16 +0100

    fix: handover & handback should update assignedAt attribute

--- api/src/chat/repositories/subscriber.repository.ts
+++ api/src/chat/repositories/subscriber.repository.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -169,6 +169,7 @@ export class SubscriberRepository extends BaseRepository<
       },
       {
         assignedTo: null,
+        assignedAt: null,
       },
     );
   }
@@ -192,6 +193,7 @@ export class SubscriberRepository extends BaseRepository<
       },
       {
         assignedTo: userId,
+        assignedAt: new Date(),
       },
     );
   }
