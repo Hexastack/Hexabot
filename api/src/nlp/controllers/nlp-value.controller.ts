16e34531b0ca2c725c027f541a2a88fe062da44b
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-01-15 11:43:24 +0100

    fix: NLP Value update DTO

--- api/src/nlp/controllers/nlp-value.controller.ts
+++ api/src/nlp/controllers/nlp-value.controller.ts
@@ -168,7 +168,10 @@ export class NlpValueController extends BaseController<
     @Param('id') id: string,
     @Body() updateNlpValueDto: NlpValueUpdateDto,
   ): Promise<NlpValue> {
-    const result = await this.nlpValueService.updateOne(id, updateNlpValueDto);
+    const result = await this.nlpValueService.updateOne(id, {
+      ...updateNlpValueDto,
+      entity: updateNlpValueDto.entity || undefined,
+    });
     if (!result) {
       this.logger.warn(`Unable to update NLP Value by id ${id}`);
       throw new NotFoundException(`NLP Value with ID ${id} not found`);
