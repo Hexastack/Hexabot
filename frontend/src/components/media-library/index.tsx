dbf9d52a1199f055a09ac53b517e819e8f0f92fe
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-01-25 14:58:15 +0100

    fix: format unused files

--- frontend/src/components/media-library/index.tsx
+++ frontend/src/components/media-library/index.tsx
@@ -6,6 +6,7 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
+
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 import { Box, Grid, Paper } from "@mui/material";
 import { GridColDef, GridEventListener } from "@mui/x-data-grid";
--- frontend/src/components/visual-editor/constants.ts
+++ frontend/src/components/visual-editor/constants.ts
@@ -6,6 +6,7 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
+
 import { IBlockAttributes } from "@/types/block.types";
 import {
   ButtonType,
