83c85c31a7eca0f719edddb97054dcf87d2244cd
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-01-29 13:01:22 +0100

    fix: update useTabUuid

--- widget/src/hooks/useTabUuid.ts
+++ widget/src/hooks/useTabUuid.ts
@@ -24,6 +24,7 @@ export const useTabUuid = (key: string = "tab_uuid") => {
       sessionStorage.setItem(key, newUuid);
       tabUuidRef.current = newUuid;
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return tabUuidRef;
