79cb85f74f4ac5778ff646189a8a2c11dea04da3
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-01-29 14:50:05 +0100

    fix: logout disconnection sockets

--- api/types/event-emitter.d.ts
+++ api/types/event-emitter.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -162,7 +162,7 @@ declare module '@nestjs/event-emitter' {
     model: TDefinition<Model>;
     permission: TDefinition<Permission>;
     role: TDefinition<Role>;
-    user: TDefinition<User, { lastvisit: Subscriber }>;
+    user: TDefinition<User, { lastvisit: Subscriber; logout: string }>;
   }
 
   /* entities hooks having schemas */
