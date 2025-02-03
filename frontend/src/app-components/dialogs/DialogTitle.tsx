8cc57d05991d9c12e11070882b7520fc48c08564
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-02-03 14:52:24 +0100

    fix(frontend): add retrocompatibility for dialog close button

--- frontend/src/app-components/dialogs/DialogTitle.tsx
+++ frontend/src/app-components/dialogs/DialogTitle.tsx
@@ -1,10 +1,11 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
+
 
 import CloseIcon from "@mui/icons-material/Close";
 import {
@@ -22,9 +23,11 @@ const StyledDialogTitle = styled(Typography)(() => ({
 export const DialogTitle = ({
   children,
   onClose,
+  onCloseV2,
 }: {
   children: React.ReactNode;
-  onClose?:
+  onClose?: () => void;
+  onCloseV2?:
     | ((event: {}, reason: "backdropClick" | "escapeKeyDown") => void)
     | undefined;
 }) => (
@@ -34,7 +37,14 @@ export const DialogTitle = ({
       <IconButton
         size="small"
         aria-label="close"
-        onClick={(e) => onClose(e, "backdropClick")}
+        onClick={(e) => {
+          if (onCloseV2) {
+            onCloseV2(e, "backdropClick");
+          } else {
+            //TODO: the old onClose prop can be replaced by the new one after the full implementation of the useDialogs hook
+            onClose();
+          }
+        }}
       >
         <CloseIcon />
       </IconButton>
