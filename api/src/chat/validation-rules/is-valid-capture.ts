e341f2d3da2adcb39820da4fdac3d0afc34917f7
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-02-03 14:37:55 +0100

    feat: replace joi with zod captureVar

--- api/src/chat/validation-rules/is-valid-capture.ts
+++ api/src/chat/validation-rules/is-valid-capture.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -12,43 +12,17 @@ import {
   ValidatorConstraint,
   ValidatorConstraintInterface,
 } from 'class-validator';
-import Joi from 'joi';
 
-type Tentity = -1 | -2;
-
-export interface CaptureVar {
-  // entity=`-1` to match text message
-  // entity=`-2` for postback payload
-  // entity is `String` for NLP entities
-  entity: Tentity | string;
-  context_var: string;
-}
-
-const allowedEntityValues: Tentity[] = [-1, -2];
+import { CaptureVar, captureVarSchema } from '../schemas/types/capture-var';
 
 export function isValidVarCapture(vars: CaptureVar[]) {
-  const captureSchema = Joi.array().items(
-    Joi.object().keys({
-      entity: Joi.alternatives().try(
-        // `-1` to match text message & `-2` for postback payload
-        Joi.number()
-          .valid(...allowedEntityValues)
-          .required(),
-        // String for NLP entities
-        Joi.string().required(),
-      ),
-      context_var: Joi.string()
-        .regex(/^[a-z][a-z_0-9]*$/)
-        .required(),
-    }),
-  );
-
-  const captureCheck = captureSchema.validate(vars);
-  if (captureCheck.error) {
-    // eslint-disable-next-line
-    console.log('Capture vars validation failed!', captureCheck.error);
+  if (!Array.isArray(vars)) {
+    return false;
   }
-  return !captureCheck.error;
+
+  return vars.every(
+    (captureVar) => captureVarSchema.safeParse(captureVar).success,
+  );
 }
 
 @ValidatorConstraint({ async: false })
