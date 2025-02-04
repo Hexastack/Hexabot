0b19f45b59b52e40dbf3f82b6c02528a64b48bc9
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-02-04 06:10:11 +0100

    fix(frontend): apply feedback updates

--- frontend/src/app-components/buttons/FormButtons.tsx
+++ frontend/src/app-components/buttons/FormButtons.tsx
@@ -1,34 +1,32 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
+
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 
 import { useTranslate } from "@/hooks/useTranslate";
 import { FormButtonsProps } from "@/types/common/dialogs.types";
 
-export const FormButtons = <T,>({
+export const DialogFormButtons = <T,>({
   onCancel,
-  onConfirm,
+  onSubmit,
 }: FormButtonsProps<T>) => {
   const { t } = useTranslate();
 
   return (
-    <>
-      <Button
-        type="submit"
-        variant="contained"
-        onClick={onConfirm}
-        startIcon={<CheckIcon />}
-      >
-        {t("button.submit")}
-      </Button>
+    <Grid
+      p="5px 15px"
+      width="100%"
+      display="flex"
+      justifyContent="space-between"
+    >
       <Button
         color="error"
         variant="outlined"
@@ -37,6 +35,14 @@ export const FormButtons = <T,>({
       >
         {t("button.cancel")}
       </Button>
-    </>
+      <Button
+        type="button"
+        variant="contained"
+        onClick={onSubmit}
+        startIcon={<CheckIcon />}
+      >
+        {t("button.submit")}
+      </Button>
+    </Grid>
   );
 };
