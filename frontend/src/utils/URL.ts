4574f2e229c5726756485b4fb4c6d05a6d842f84
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-02-06 17:14:38 +0100

    fix(frontend): resolve isAbsoluteUrl bug

--- frontend/src/utils/URL.ts
+++ frontend/src/utils/URL.ts
@@ -1,10 +1,11 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
+
 
 export const getFromQuery = ({
   key,
@@ -48,6 +49,7 @@ export const isAbsoluteUrl = (value: string = ""): boolean => {
 
     return (
       (url.protocol === "http:" || url.protocol === "https:") &&
+      (url.href.startsWith("http://") || url.href.startsWith("https://")) &&
       hostnameParts.length > 1 &&
       hostnameParts[hostnameParts.length - 1].length > 1
     );
