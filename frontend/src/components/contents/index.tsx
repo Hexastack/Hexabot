3124d3bbf0a6ff18c200eefa3ee905d3ea767dff
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-02-07 22:57:56 +0100

    refactor(frontend): update content and contentImport dialogs

--- frontend/src/components/contents/index.tsx
+++ frontend/src/components/contents/index.tsx
@@ -1,22 +1,20 @@
 /*
- * Copyright © 2025 Hexastack. All rights reserved.
+ * Copyright © 2024 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
-
 import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
 import AddIcon from "@mui/icons-material/Add";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import UploadIcon from "@mui/icons-material/Upload";
 import { Button, Chip, Grid, Paper, Switch, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useQueryClient } from "react-query";
 
-import { DeleteDialog } from "@/app-components/dialogs";
-import FileUploadButton from "@/app-components/inputs/FileInput";
+import { ConfirmDialogBody } from "@/app-components/dialogs";
 import { FilterTextfield } from "@/app-components/inputs/FilterTextfield";
 import {
   ActionColumnLabel,
@@ -24,57 +22,42 @@ import {
 } from "@/app-components/tables/columns/getColumns";
 import { renderHeader } from "@/app-components/tables/columns/renderHeader";
 import { DataGrid } from "@/app-components/tables/DataGrid";
-import { isSameEntity } from "@/hooks/crud/helpers";
 import { useDelete } from "@/hooks/crud/useDelete";
 import { useFind } from "@/hooks/crud/useFind";
 import { useGet, useGetFromCache } from "@/hooks/crud/useGet";
-import { useImport } from "@/hooks/crud/useImport";
 import { useUpdate } from "@/hooks/crud/useUpdate";
-import { getDisplayDialogs, useDialog } from "@/hooks/useDialog";
+import { useDialogs } from "@/hooks/useDialogs";
 import { useHasPermission } from "@/hooks/useHasPermission";
 import { useSearch } from "@/hooks/useSearch";
 import { useToast } from "@/hooks/useToast";
 import { useTranslate } from "@/hooks/useTranslate";
 import { PageHeader } from "@/layout/content/PageHeader";
 import { EntityType, Format } from "@/services/types";
-import { IContentType } from "@/types/content-type.types";
 import { IContent } from "@/types/content.types";
 import { PermissionAction } from "@/types/permission.types";
 import { getDateTimeFormatter } from "@/utils/date";
 
-import { ContentDialog } from "./ContentDialog";
+import { ContentFormDialog } from "./ContentFormDialog";
+import { ContentImportFormDialog } from "./ContentImportFormDialog";
 
 export const Contents = () => {
   const { t } = useTranslate();
   const { toast } = useToast();
   const { query } = useRouter();
-  const queryClient = useQueryClient();
-  // Dialog Controls
-  const addDialogCtl = useDialog<{
-    content?: IContent;
-    contentType?: IContentType;
-  }>(false);
-  const editDialogCtl = useDialog<{
-    content?: IContent;
-    contentType?: IContentType;
-  }>(false);
-  const deleteDialogCtl = useDialog<string>(false);
+  const dialogs = useDialogs();
   // data fetching
   const { onSearch, searchPayload } = useSearch<IContent>({
     $eq: [{ entity: String(query.id) }],
     $iLike: ["title"],
   });
   const hasPermission = useHasPermission();
-  const { data: contentType } = useGet(String(query.id), {
-    entity: EntityType.CONTENT_TYPE,
-  });
-  const { dataGridProps } = useFind(
+  const { dataGridProps, refetch } = useFind(
     { entity: EntityType.CONTENT, format: Format.FULL },
     {
       params: searchPayload,
     },
   );
-  const { mutateAsync: updateContent } = useUpdate(EntityType.CONTENT, {
+  const { mutate: updateContent } = useUpdate(EntityType.CONTENT, {
     onError: (error) => {
       toast.error(error.message || t("message.internal_server_error"));
     },
@@ -82,9 +65,8 @@ export const Contents = () => {
       toast.success(t("message.success_save"));
     },
   });
-  const { mutateAsync: deleteContent } = useDelete(EntityType.CONTENT, {
+  const { mutate: deleteContent } = useDelete(EntityType.CONTENT, {
     onSuccess: () => {
-      deleteDialogCtl.closeDialog();
       toast.success(t("message.item_delete_success"));
     },
   });
@@ -94,52 +76,27 @@ export const Contents = () => {
     [
       {
         label: ActionColumnLabel.Edit,
-        action: (content) =>
-          editDialogCtl.openDialog({
-            contentType,
-            content,
-          }),
+        action: (row) =>
+          dialogs.open(ContentFormDialog, { content: row, contentType }),
         requires: [PermissionAction.UPDATE],
       },
       {
         label: ActionColumnLabel.Delete,
-        action: (content) => deleteDialogCtl.openDialog(content.id),
+        action: async ({ id }) => {
+          const isConfirmed = await dialogs.confirm(ConfirmDialogBody);
+
+          if (isConfirmed) {
+            deleteContent(id);
+          }
+        },
         requires: [PermissionAction.DELETE],
       },
     ],
     t("label.operations"),
   );
-  const { data } = useGet(String(query.id), {
+  const { data: contentType } = useGet(String(query.id), {
     entity: EntityType.CONTENT_TYPE,
   });
-  const { mutateAsync: importDataset, isLoading } = useImport(
-    EntityType.CONTENT,
-    {
-      onError: () => {
-        toast.error(t("message.import_failed"));
-      },
-      onSuccess: (data) => {
-        queryClient.removeQueries({
-          predicate: ({ queryKey }) => {
-            const [_qType, qEntity] = queryKey;
-
-            return (
-              isSameEntity(qEntity, EntityType.CONTENT)
-            );
-          },
-        });
-        if (data.length) {
-          toast.success(t("message.success_import"));
-        } else {
-          toast.error(t("message.import_duplicated_data"));
-        }
-      },
-    },
-    { idTargetContentType: contentType?.id }
-  );
-  const handleImportChange = async (file: File) => {
-    await importDataset(file);
-  };
 
   return (
     <Grid container flexDirection="column" gap={3}>
@@ -152,7 +109,9 @@ export const Contents = () => {
 
         <PageHeader
           icon={faAlignLeft}
-          chip={<Chip label={data?.name} size="medium" variant="title" />}
+          chip={
+            <Chip label={contentType?.name} size="medium" variant="title" />
+          }
           title={t("title.content")}
         >
           <Grid justifyContent="flex-end" gap={1} container alignItems="center">
@@ -164,7 +123,9 @@ export const Contents = () => {
                 <Button
                   startIcon={<AddIcon />}
                   variant="contained"
-                  onClick={() => addDialogCtl.openDialog({ contentType })}
+                  onClick={() =>
+                    dialogs.open(ContentFormDialog, { contentType })
+                  }
                   sx={{ float: "right" }}
                 >
                   {t("button.add")}
@@ -173,13 +134,22 @@ export const Contents = () => {
             ) : null}
             {hasPermission(EntityType.CONTENT, PermissionAction.CREATE) ? (
               <Grid item>
-                <FileUploadButton
-                accept="text/csv"
-                label={t("button.import")}
-                onChange={handleImportChange}
-                isLoading={isLoading}
-              />
-            
+                <Button
+                  startIcon={<UploadIcon />}
+                  variant="contained"
+                  onClick={async () => {
+                    if (contentType) {
+                      await dialogs.open(ContentImportFormDialog, {
+                        row: null,
+                        contentType,
+                      });
+                      refetch();
+                    }
+                  }}
+                  sx={{ float: "right" }}
+                >
+                  {t("button.import")}
+                </Button>
               </Grid>
             ) : null}
           </Grid>
@@ -187,15 +157,6 @@ export const Contents = () => {
       </Grid>
       <Grid item>
         <Paper>
-          <ContentDialog {...getDisplayDialogs(addDialogCtl)} />
-          <ContentDialog {...getDisplayDialogs(editDialogCtl)} />
-          <DeleteDialog
-            {...deleteDialogCtl}
-            callback={() => {
-              if (deleteDialogCtl?.data) deleteContent(deleteDialogCtl.data);
-            }}
-          />
-
           <Grid padding={2} container>
             <Grid item width="100%">
               <DataGrid<IContent>
