8184abb0598132e33c7cd7d4b9d7661407c38bb3
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-02-09 12:21:26 +0100

    feat(frontend): introduce ESLint no-multiple-empty-lines rule

--- frontend/src/components/contents/ContentImportDialog.tsx
+++ frontend/src/components/contents/ContentImportDialog.tsx
@@ -6,7 +6,6 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
-
 import CloseIcon from "@mui/icons-material/Close";
 import { Button, Dialog, DialogActions, DialogContent } from "@mui/material";
 import { FC, useState } from "react";
--- frontend/src/components/visual-editor/v2/AdvancedLink/AdvancedLinkFactory.tsx
+++ frontend/src/components/visual-editor/v2/AdvancedLink/AdvancedLinkFactory.tsx
@@ -6,7 +6,6 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
-
 import { css, keyframes } from "@emotion/react";
 import styled from "@emotion/styled";
 import {
