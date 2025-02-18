4fb34206ad44a8f8b7a510b73538c298316d3e22
hexastack <talebhassan.ikbel@gmail.com>
2025-02-18 18:49:17 +0100

    fix: edit nlp sample edit language

--- frontend/src/app-components/inputs/AutoCompleteEntitySelect.tsx
+++ frontend/src/app-components/inputs/AutoCompleteEntitySelect.tsx
@@ -1,10 +1,11 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
+
 
 import { ChipTypeMap } from "@mui/material";
 import { AutocompleteProps } from "@mui/material/Autocomplete";
@@ -74,14 +75,7 @@ const AutoCompleteEntitySelect = <
   const idRef = useRef(generateId());
   const params = {
     where: {
-      or: [
-        ...(searchPayload.where.or || []),
-        ...(value
-          ? Array.isArray(value)
-            ? value.map((v) => ({ [idKey]: v }))
-            : [{ [idKey]: value }]
-          : []),
-      ],
+      or: [...(searchPayload.where.or || [])],
     },
   };
   const { data, isFetching, fetchNextPage } = useInfiniteFind(
