67200b609d493b244e6e89387eb12d4ffa6498f4
hexastack <talebhassan.ikbel@gmail.com>
2025-02-19 07:19:59 +0100

    fix(lint): remove extra empty line

--- frontend/src/app-components/inputs/AutoCompleteEntitySelect.tsx
+++ frontend/src/app-components/inputs/AutoCompleteEntitySelect.tsx
@@ -6,7 +6,6 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
-
 import { ChipTypeMap } from "@mui/material";
 import { AutocompleteProps } from "@mui/material/Autocomplete";
 import { forwardRef, useEffect, useRef } from "react";
