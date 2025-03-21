73561ada7e85235313485f5aa94572c93ed1b94d
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-03-21 15:49:27 +0100

    fix: move findAndPopulateNlpValuesWithCount to the repository

--- api/src/nlp/repositories/nlp-value.repository.ts
+++ api/src/nlp/repositories/nlp-value.repository.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -9,9 +9,10 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { InjectModel } from '@nestjs/mongoose';
-import { Document, Model, Query } from 'mongoose';
+import { Document, Model, Query, Types } from 'mongoose';
 
 import { BaseRepository, DeleteResult } from '@/utils/generics/base-repository';
+import { PageQueryDto } from '@/utils/pagination/pagination-query.dto';
 import { TFilterQuery } from '@/utils/types/filter.types';
 
 import { NlpValueDto } from '../dto/nlp-value.dto';
@@ -107,5 +108,119 @@ export class NlpValueRepository extends BaseRepository<
     } else {
       throw new Error('Attempted to delete a NLP value using unknown criteria');
     }
+  }
+
+  async findAndPopulateNlpValuesWithCount(
+    populate: string[],
+    filters?: TFilterQuery<NlpValue>,
+    pageQuery?: PageQueryDto<NlpValue>,
+  ) {
+    const { $and = [], ...rest } = filters || ({} as TFilterQuery<NlpValue>);
+
+    return this.model
+      .aggregate<NlpValue>([
+        {
+          // support filters
+          $match: {
+            ...rest,
+            ...($and?.length && {
+              $and:
+                $and?.map((v) => {
+                  if (v.entity) {
+                    return {
+                      ...v,
+                      entity: new Types.ObjectId(String(v.entity)),
+                    };
+                  }
+
+                  return v;
+                }) || [],
+            }),
+          },
+        },
+        // support pageQuery
+        {
+          $skip: pageQuery?.skip || 0,
+        },
+        {
+          $limit: pageQuery?.limit || 10,
+        },
+        {
+          $sort: {
+            [pageQuery?.sort?.[0] || 'createdAt']:
+              pageQuery?.sort?.[1] === 'desc' ? -1 : 1,
+          },
+        },
+        {
+          $lookup: {
+            from: 'nlpsampleentities',
+            localField: '_id',
+            foreignField: 'value',
+            as: 'sampleEntities',
+          },
+        },
+        {
+          $unwind: {
+            path: '$sampleEntities',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'nlpsamples',
+            localField: 'sampleEntities.sample',
+            foreignField: '_id',
+            as: 'samples',
+          },
+        },
+        {
+          $lookup: {
+            from: 'nlpentities',
+            localField: 'entity',
+            foreignField: '_id',
+            as: 'entities',
+          },
+        },
+        {
+          $group: {
+            _id: '$_id',
+            value: { $first: '$value' },
+            expressions: { $first: '$expressions' },
+            builtin: { $first: '$builtin' },
+            metadata: { $first: '$metadata' },
+            createdAt: { $first: '$createdAt' },
+            updatedAt: { $first: '$updatedAt' },
+            entity: {
+              // support populate
+              $first: populate.some((p) =>
+                this.getPopulate()
+                  .map((p) => p.toString())
+                  .includes(p),
+              )
+                ? '$entities'
+                : '$entity',
+            },
+            //TODO when samples is empty array we need to return 0 not 1
+            nlpSamplesCount: {
+              $sum: { $cond: [{ $ifNull: ['samples', false] }, 1, 0] },
+            },
+          },
+        },
+        {
+          $project: {
+            id: '$_id',
+            _id: 0,
+            value: 1,
+            expressions: 1,
+            builtin: 1,
+            entity: 1,
+            metadata: 1,
+            createdAt: 1,
+            updatedAt: 1,
+            nlpSamplesCount: 1,
+          },
+        },
+      ])
+      .exec();
   }
 }
