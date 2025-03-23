50593bd8a9ce9c844f558b0abea95be1a0073982
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-03-23 15:14:13 +0100

    fix: rename findAndPopulate method

--- api/src/nlp/controllers/nlp-value.controller.ts
+++ api/src/nlp/controllers/nlp-value.controller.ts
@@ -128,7 +128,7 @@ export class NlpValueController extends BaseController<
   }
 
   @Get('')
-  async findAndPopulateNlpValuesWithCount(
+  async findAndPopulateWithCount(
     @Query(PageQueryPipe) pageQuery: PageQueryDto<NlpValue>,
     @Query(PopulatePipe) populate: string[],
     @Query(
@@ -136,7 +136,7 @@ export class NlpValueController extends BaseController<
     )
     filters: TFilterQuery<NlpValue>,
   ) {
-    return await this.nlpValueService.findAndPopulateNlpValuesWithCount(
+    return await this.nlpValueService.findAndPopulateWithCount(
       pageQuery,
       populate,
       filters,
