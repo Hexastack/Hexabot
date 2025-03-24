408d50a5d38ed14e36f623d5904f81d7ae7c8ec7
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-03-24 11:56:52 +0100

    fix: base plugin getDefaultSettings

--- api/src/plugins/base-block-plugin.ts
+++ api/src/plugins/base-block-plugin.ts
@@ -38,7 +38,7 @@ export abstract class BaseBlockPlugin<
     this.settings = require(path.join(this.getPath(), 'settings')).default;
   }
 
-  async getDefaultSettings(): Promise<T> {
+  getDefaultSettings(): Promise<T> | T {
     return this.settings;
   }
 
