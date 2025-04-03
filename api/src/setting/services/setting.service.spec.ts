7405eca14ecbe251bd4b49371b42a8948de50439
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-04-03 17:34:26 +0100

    fix: settings tests

--- api/src/setting/services/setting.service.spec.ts
+++ api/src/setting/services/setting.service.spec.ts
@@ -164,14 +164,13 @@ describe('SettingService', () => {
       expect(settingService.find).toHaveBeenCalledWith({
         label: 'allowed_domains',
       });
-      expect(result).toEqual(
-        new Set([
-          '*',
-          'https://example.com',
-          'https://test.com',
-          'https://another.com',
-        ]),
-      );
+      expect(result).toEqual([
+        '*',
+        'https://example.com',
+        'https://test.com',
+        'https://example.com',
+        'https://another.com',
+      ]);
     });
 
     it('should return the config allowed cors only if no settings are found', async () => {
@@ -182,7 +181,7 @@ describe('SettingService', () => {
       expect(settingService.find).toHaveBeenCalledWith({
         label: 'allowed_domains',
       });
-      expect(result).toEqual(new Set(['*']));
+      expect(result).toEqual(['*']);
     });
 
     it('should handle settings with empty values', async () => {
@@ -198,7 +197,7 @@ describe('SettingService', () => {
       expect(settingService.find).toHaveBeenCalledWith({
         label: 'allowed_domains',
       });
-      expect(result).toEqual(new Set(['*', 'https://example.com']));
+      expect(result).toEqual(['*', 'https://example.com']);
     });
   });
 });
