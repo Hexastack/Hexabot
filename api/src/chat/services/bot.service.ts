5be637fd30a23bbf7cee25cfb5f8db24693cbecd
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-04-20 11:53:42 +0100

    fix(api): resolve block local fallback issue

--- api/src/chat/services/bot.service.ts
+++ api/src/chat/services/bot.service.ts
@@ -275,7 +275,6 @@ export class BotService {
         !matchedBlock &&
         event.getMessageType() === IncomingMessageType.message &&
         fallbackOptions.active &&
-        convo.context?.attempt &&
         convo.context.attempt < fallbackOptions.max_attempts
       ) {
         // Trigger block fallback
