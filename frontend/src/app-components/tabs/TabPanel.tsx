e30f5eaef3dc516e8fcdc7adabbedbd33a7e2cf6
hexastack <talebhassan.ikbel@gmail.com>
2025-04-24 18:43:07 +0100

    fix: validation bypass when updating block

--- frontend/src/app-components/tabs/TabPanel.tsx
+++ frontend/src/app-components/tabs/TabPanel.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -24,16 +24,21 @@ export function TabPanel(props: TabPanelProps & GridProps) {
       item
       xs
       role="tabpanel"
-      hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
+      sx={{
+        display: "block",
+        visibility: value === index ? "visible" : "hidden",
+        height: value === index ? "auto" : 0,
+        overflow: "hidden",
+        position: value === index ? "relative" : "absolute",
+        ...sx,
+      }}
       {...other}
     >
-      {value === index ? (
-        <Grid container sx={{ flexDirection: "column", ...sx }}>
-          {children}
-        </Grid>
-      ) : null}
+      <Grid container sx={{ flexDirection: "column" }}>
+        {children}
+      </Grid>
     </Grid>
   );
 }
