917d497b89ec4392bd3856f42e83192d1bade50d
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-05-01 15:58:31 +0100

    fix: connectionLost ui

--- widget/src/components/icons/LoadingIcon.tsx
+++ widget/src/components/icons/LoadingIcon.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -23,7 +23,7 @@ const LoadingIcon: FC<
       fill={color}
       {...rest}
     >
-      <circle cx="25" cy="25" r="20" stroke="none" fill="none">
+      <circle cx="25" cy="25" r="20" stroke="none" fill={color}>
         <animate
           attributeName="r"
           begin="0s"
