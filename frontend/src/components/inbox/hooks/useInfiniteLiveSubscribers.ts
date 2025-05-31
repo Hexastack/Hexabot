f1c7d889c31f88fbdc9d59a4a07e54f551b75cca
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-05-31 17:30:10 +0100

    fix(frontend): resolve useInfiniteLiveSubscribers hook or operator issue

--- frontend/src/components/inbox/hooks/useInfiniteLiveSubscribers.ts
+++ frontend/src/components/inbox/hooks/useInfiniteLiveSubscribers.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -29,14 +29,15 @@ export const useInfiniteLiveSubscribers = (props: {
   const queryClient = useQueryClient();
   const params = {
     where: {
-      ...(props.channels.length > 0
-        ? {
-            or: props.channels.map((channel) => ({
-              "channel.name": channel,
-            })),
-          }
-        : {}),
       ...props.searchPayload.where,
+      ...(props.channels.length > 0 && {
+        or: [
+          ...props.searchPayload.where.or,
+          ...props.channels.map((channel) => ({
+            "channel.name": channel,
+          })),
+        ],
+      }),
       ...(props.assignedTo === AssignedTo.ME
         ? { assignedTo: user?.id }
         : props.assignedTo === AssignedTo.OTHERS
