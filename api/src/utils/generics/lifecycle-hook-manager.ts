07be5c6fd06ce73d4700b7b231ff99fc4401c609
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-06-16 09:50:39 +0100

    fix(api): apply feedback

--- api/src/utils/generics/lifecycle-hook-manager.ts
+++ api/src/utils/generics/lifecycle-hook-manager.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -46,11 +46,11 @@ export class LifecycleHookManager {
   }
 
   private static addModel(model: ModelDefinition) {
-    if (this.models.has(model.name)) {
+    if (LifecycleHookManager.models.has(model.name)) {
       throw new Error(`Model with name ${model} already exists`);
     }
 
-    this.models.set(model.name, model);
+    LifecycleHookManager.models.set(model.name, model);
   }
 
   private static createLifecycleCallback<H = PreHook | PostHook>(): H {
