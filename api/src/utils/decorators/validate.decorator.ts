5256e94ebc60278ad5850cb1207f06750e4aec77
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-07-12 13:13:00 +0100

    feat(api): add validate decorator

---
+++ api/src/utils/decorators/validate.decorator.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2025 Hexastack. All rights reserved.
+ *
+ * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
+ * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
+ * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
+ */
+
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+import { ZodType } from 'zod';
+
+export const Validate =
+  (schema: ZodType, validationOptions?: ValidationOptions) =>
+  (object: object, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: {
+        validate(data: unknown, _validationArguments?: ValidationArguments) {
+          if (Array.isArray(data)) {
+            return data.every((datum) => schema.safeParse(datum).success);
+          }
+
+          return schema.safeParse(data).success;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return (
+            schema
+              ?.safeParse?.(args.value)
+              ?.error?.errors.map((e) => e.message)
+              .join(', ') || 'Validation failed'
+          );
+        },
+      },
+    });
+  };
