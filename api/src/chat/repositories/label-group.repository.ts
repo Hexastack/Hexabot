d7d3405de07567100b9fbc6e9afc33d6345907ed
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2025-07-30 12:28:18 +0100

    fix: add cascade delete logic

--- api/src/chat/repositories/label-group.repository.ts
+++ api/src/chat/repositories/label-group.repository.ts
@@ -8,12 +8,14 @@
 
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Document, Model, Query } from 'mongoose';
 
-import { BaseRepository } from '@/utils/generics/base-repository';
+import { BaseRepository, DeleteResult } from '@/utils/generics/base-repository';
+import { TFilterQuery } from '@/utils/types/filter.types';
 
 import { LabelGroupDto } from '../dto/label-group.dto';
 import { LabelGroup } from '../schemas/label-group.schema';
+import { LabelService } from '../services/label.service';
 
 @Injectable()
 export class LabelGroupRepository extends BaseRepository<
@@ -22,7 +24,36 @@ export class LabelGroupRepository extends BaseRepository<
   never,
   LabelGroupDto
 > {
-  constructor(@InjectModel(LabelGroup.name) readonly model: Model<LabelGroup>) {
+  constructor(
+    @InjectModel(LabelGroup.name) readonly model: Model<LabelGroup>,
+    private readonly labelService: LabelService,
+  ) {
     super(model, LabelGroup);
+  }
+
+  /**
+   * Reset all related labels group to `null` to ensure consistency.
+   *
+   * @param query - The delete query.
+   * @param criteria - The filter criteria for finding groups to delete.
+   */
+  async preDelete(
+    _query: Query<
+      DeleteResult,
+      Document<LabelGroup, any, any>,
+      unknown,
+      LabelGroup,
+      'deleteOne' | 'deleteMany'
+    >,
+    criteria: TFilterQuery<LabelGroup>,
+  ) {
+    const groups = await this.find(criteria);
+
+    await this.labelService.updateMany(
+      {
+        group: { $in: groups.map(({ id }) => id) },
+      },
+      { group: null },
+    );
   }
 }
