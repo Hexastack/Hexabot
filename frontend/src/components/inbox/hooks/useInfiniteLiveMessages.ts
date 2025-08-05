66f32d7656f69b1640d585ca3385e86e3fa3e11e
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-08-05 17:00:54 +0100

    fix(frontend): resolve unsynchronized inbox widget messages

--- frontend/src/components/inbox/hooks/useInfiniteLiveMessages.ts
+++ frontend/src/components/inbox/hooks/useInfiniteLiveMessages.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
  * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
@@ -57,7 +57,7 @@ export const useInfinitedLiveMessages = () => {
         const { result } = normalizeAndCache(event.msg);
 
         queryClient.setQueryData(
-          [QueryType.infinite, EntityType.MESSAGE, params],
+          [QueryType.infinite, EntityType.MESSAGE, JSON.stringify(params)],
           (oldData) => {
             if (oldData) {
               const data = oldData as InfiniteData<string[]>;
