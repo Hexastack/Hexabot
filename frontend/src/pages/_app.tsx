ab64644f88a35bae296ad0b9f61b1f5cb7de0632
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-10-01 08:15:31 +0100

    fix(frontend): resolve build issue

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -29,6 +29,7 @@ import { theme } from "@/layout/themes/theme";
 import { SocketProvider } from "@/websocket/socket-hooks";
 
 import "../components/inbox/inbox.css";
+import "../components/visual-editor/v3/styles/index.css";
 import "../i18n/config";
 import "../styles/globals.css";
 import "../styles/visual-editor.css";
