3ceb4ca64e7ba36cc183dfe43844b7fd1cb77210
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2025-12-02 09:01:30 +0100

    fix(api): apply feedback

--- packages/api/test/global-setup.ts
+++ packages/api/test/global-setup.ts
@@ -7,19 +7,37 @@
 import net from 'net';
 import 'tsconfig-paths/register';
 
-const API_PORT = 3000;
-const isReservedPort = async (port: number) =>
-  new Promise((resolve) => {
+const API_PORT = Number(process.env.API_PORT ?? 3000);
+
+function isPortInUse(port: number): Promise<boolean> {
+  return new Promise((resolve, reject) => {
     const server = net.createServer();
+    const onError = (err: NodeJS.ErrnoException) => {
+      // Typical “port already in use”
+      if (err.code === 'EADDRINUSE') {
+        resolve(true);
+      } else {
+        reject(err);
+      }
+    };
+    const onListen = () => {
+      server.close(() => resolve(false));
+    };
+
+    server.once('error', onError);
+    server.once('listening', onListen);
+
     server.unref();
-    server.listen(port, () => {
-      server.close();
-      resolve(false);
-    });
-    server.on('error', resolve);
+    server.listen(port);
   });
+}
+
 export = async function globalSetup() {
-  if (await isReservedPort(API_PORT)) {
-    throw `Not available required port ${API_PORT}`;
+  const inUse = await isPortInUse(API_PORT);
+
+  if (inUse) {
+    throw new Error(
+      `Port ${API_PORT} is already in use. Please stop the running service before executing Jest tests.`,
+    );
   }
 };
