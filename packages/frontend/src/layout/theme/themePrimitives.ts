a79eda20db4ab259d69a936df66302e694516397
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2026-02-09 13:14:17 +0100

    fix(frontend): set roboto as default fontFamily

--- packages/frontend/src/layout/theme/themePrimitives.ts
+++ packages/frontend/src/layout/theme/themePrimitives.ts
@@ -206,7 +206,7 @@ export const colorSchemes = {
 };
 
 export const typography = {
-  fontFamily: "Inter, sans-serif",
+  fontFamily: "Roboto, Inter, sans-serif",
   h1: {
     fontSize: defaultTheme.typography.pxToRem(48),
     fontWeight: 600,
