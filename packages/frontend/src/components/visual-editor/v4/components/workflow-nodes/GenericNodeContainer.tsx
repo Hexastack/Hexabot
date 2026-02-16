98e03da221eb4c9e5dd3467747743afc33a5a851
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2026-02-16 10:16:44 +0100

    fix(frontend): update group port position

--- packages/frontend/src/components/visual-editor/v4/components/workflow-nodes/GenericNodeContainer.tsx
+++ packages/frontend/src/components/visual-editor/v4/components/workflow-nodes/GenericNodeContainer.tsx
@@ -6,11 +6,21 @@
 
 import type { PropsWithChildren } from "react";
 
+import { useWorkflowNode } from "../../hooks/useWorkflowNode";
+
 export const GenericNodeContainer = ({ children }: PropsWithChildren) => {
+  const { height, width } = useWorkflowNode();
+
   return (
     <div
       style={{
         position: "relative",
+        ...(width && height
+          ? {}
+          : {
+              minWidth: "calc(100% + 10px)",
+              minHeight: "calc(100% + 10px)",
+            }),
         padding: "5px",
       }}
     >
