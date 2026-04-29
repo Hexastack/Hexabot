2de3a69b45f878664b6fb7edd0c1c6f6a09623c7
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2026-04-29 22:48:27 +0100

    fix: silence unit test logs

--- packages/api/test/setup-tests.ts
+++ packages/api/test/setup-tests.ts
@@ -42,3 +42,21 @@ jest.mock('nestjs-dynamic-providers', () => ({
   InjectDynamicProviders: () => (target: unknown) => target,
   resolveDynamicProviders: jest.fn(async () => undefined),
 }));
+
+jest.mock('@/logger/logger.service', () => {
+  class LoggerService {
+    log = jest.fn();
+
+    error = jest.fn();
+
+    warn = jest.fn();
+
+    debug = jest.fn();
+
+    verbose = jest.fn();
+
+    fatal = jest.fn();
+  }
+
+  return { LoggerService };
+});
